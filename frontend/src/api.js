8dd67fd6a2d2756803eb3e3c0c32d6fd889bd9ba
ShaliniVijapuram27 <shalinivijapuram@gmail.com>
2026-06-07 14:15:31 +0530

    Updated api.js by adding db url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://office-backend-env.eba-rrcfekmh.us-east-1.elasticbeanstalk.com/"
+  baseURL:"http://a82a24edfbc924a7c9d15643fae68e5f-19924227.us-east-1.elb.amazonaws.com:8000"
 });
 
 export default api;
